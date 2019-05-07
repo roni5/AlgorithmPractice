dfc86727b36d6d7b5099c23bb0fe493a1b3e663f
Sungin Jung <jungs@uchicago.edu>
2019-05-08 00:49:21 +0900

    reverseString

---
+++ LeetCode/Python/reverseString.py
@@ -0,0 +1,11 @@
+#O(n) in-place reverse string
+def reverseString(s:List[str]) -> None:
+    last_idx = len(s) - 1
+    for idx in range(len(s) // 2):
+        temp = s[idx]
+        s[idx] = s[last_idx]
+        s[last_idx] = temp
+
+
+
+
