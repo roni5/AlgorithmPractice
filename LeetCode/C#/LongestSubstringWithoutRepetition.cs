03f1e1ca637e8de5474b752506221f55d29c2ac2
Sungin Jung <jungs@uchicago.edu>
2019-05-31 17:56:42 -0700

    5/31/2109

---
+++ LeetCode/C#/LongestSubstringWithoutRepetition.cs
@@ -0,0 +1,24 @@
+public int LengthOfLongestSubstring(string s) {
+
+    if(s.Length == 0) return 0;
+
+    int length = s.Length;
+    int i = 0;
+    int j = 0;
+    int answer = 0;
+    var mySet = new HashSet<char>();
+
+    while(i < 0 && j < 0) {
+        if(!mySet.Contains(s[i])){
+            mySet.Add(s[i]);
+            i += 1;
+            answer = Math.Max(answer, i - j);
+        } else {
+            mySet.Remove(s[j]);
+            j -= 1;
+        }
+    }
+
+    return answer;
+
+}
