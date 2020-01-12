bc45c384ae2501cef332cd626eb6e6d2d349e555
Sungin Jung <jungs@uchicago.edu>
2020-01-11 19:00:22 -0800

    1/10/2020

---
+++ LeetCode/Python/SecondRound/733_floodFill.py
@@ -0,0 +1,47 @@
+# An image is represented by a 2-D array of integers, each integer representing the pixel value of the image(from 0 to 65535).
+
+# Given a coordinate(sr, sc) representing the starting pixel(row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+# To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels(also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+# At the end, return the modified image.
+
+# Example 1:
+# Input:
+# image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+# sr = 1, sc = 1, newColor = 2
+# Output: [[2, 2, 2], [2, 2, 0], [2, 0, 1]]
+# Explanation:
+# From the center of the image(with position(sr, sc)=(1, 1)), all pixels connected
+# by a path of the same color as the starting pixel are colored with the new color.
+# Note the bottom corner is not colored 2, because it is not 4-directionally connected
+# to the starting pixel.
+
+
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        if image[sr][sc] == newColor:
+            return image
+
+        oldColor = image[sr][sc]
+
+        self.fill(image, sr, sc, oldColor, newColor)
+        return image
+
+    def fill(self, image, sr, sc, oldColor, newColor):
+        if(sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[0]) or image[sr][sc] != oldColor):
+            return
+
+        image[sr][sc] = newColor
+
+        self.fill(image, sr - 1, sc, oldColor, newColor)
+        self.fill(image, sr + 1, sc, oldColor, newColor)
+        self.fill(image, sr, sc - 1, oldColor, newColor)
+        self.fill(image, sr, sc + 1, oldColor, newColor)
