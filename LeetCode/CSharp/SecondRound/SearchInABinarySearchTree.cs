25e24ee98db330d41985cdf86aca1f2152e5148e
Sungin <jungs@uchicago.edu>
2021-02-28 20:50:22 -0800

    Search In a Binary Search Tree

---
+++ LeetCode/CSharp/SecondRound/SearchInABinarySearchTree.cs
@@ -0,0 +1,19 @@
+namespace LeetCode.CSharp.SecondRound
+{
+    public class SearchInABinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null) return null;
+            return SearchNode(root, val);
+        }
+
+        private TreeNode SearchNode(TreeNode root, int val)
+        {
+            if (root == null) return null;
+            if (val < root.val) return SearchNode(root.left, val);
+            else if (val > root.val) return SearchNode(root.right, val);
+            else return root;
+        }
+    }
+}
