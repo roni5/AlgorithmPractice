55d796382c920cbdad81339f6c0153dc0555f0db
Sungin <jungs@uchicago.edu>
2021-03-10 20:58:04 -0800

    Climbing Stairs

---
+++ LeetCode/CSharp/SecondRound/ClimbingStairs.cs
@@ -0,0 +1,21 @@
+namespace LeetCode.CSharp.SecondRound
+{
+    public class ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1 || n == 2) return n;
+
+            int[] dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
