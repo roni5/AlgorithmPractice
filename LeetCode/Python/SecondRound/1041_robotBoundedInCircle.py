0c01e0a1450a3c9d9a9c682fa0b0a638f4d58e18
Sungin <jungs@uchicago.edu>
2021-03-11 17:16:41 -0800

    Python_Robot Bounded In Circle

---
+++ LeetCode/Python/SecondRound/1041_robotBoundedInCircle.py
@@ -0,0 +1,15 @@
+def isRobotBounded(self, instructions: str) -> bool:
+
+    direction = (0,1)
+    start = [0,0]
+
+    for instruction in instructions:
+        if instruction == "G":
+            start[0] += direction[0]
+            start[1] += direction[1]
+        elif instruction == "L":
+            direction = (-direction[1], direction[0])
+        elif instruction == "R":
+            direction = (direction[1], -direction[0])
+    
+    return start == [0,0] or direction != (0, 1)
