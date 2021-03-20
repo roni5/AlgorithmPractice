edb1f50cea5bce9ada098963f642b34404135169
Sungin <jungs@uchicago.edu>
2021-03-20 03:43:32 -0700

    Python_Inorder Successor BST

---
+++ LeetCode/Python/SecondRound/285_inorderSuccessorBST.py
@@ -0,0 +1,13 @@
+def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+
+    successor = None
+
+    while root:
+        if p.val >= root.val:
+            root = root.right
+        else:
+            successor = root
+            root = root.left
+    
+    return successor
+    
