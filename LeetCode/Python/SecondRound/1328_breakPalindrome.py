14c3fadbd62bd1d1be283bd8f007d2f6b799004d
Sungin Jung <Sungin.Jung@ey.com>
2021-04-07 22:47:11 -0700

    Python_Break Palindrome fix

--- LeetCode/Python/SecondRound/1328_breakPalindrome.py
+++ LeetCode/Python/SecondRound/1328_breakPalindrome.py
@@ -1,18 +1,19 @@
 def breakPalindrome(self, palindrome: str) -> str:
 
     if len(palindrome) == 0 or len(palindrome) == 1:
-        return None
+        return ""
     
     start = 0
     end = len(palindrome) - 1
     newPal = list(palindrome)
-
-    for idx, letter in enumerate(newPal):
-        if letter != 'a':
-            newPal[idx] = 'a'
+    
+    while start < end:
+        if newPal[start] != "a":
+            newPal[start] = "a"
             return "".join(newPal)
         start += 1
         end -= 1
+        
+    newPal[-1] = "b"
     
-    newPal[-1] = 'b'
     return "".join(newPal)
