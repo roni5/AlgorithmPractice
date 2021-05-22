10e4a1a94e3da25aa5bed3d7cce0d2725a46aa63
Sungin <jungs@uchicago.edu>
2021-05-21 20:01:17 -0700

    Python_Maximum Depth of Binary Tree

---
+++ LeetCode/Python/SecondRound/104_maximumDepthOfBinaryTree.py
@@ -0,0 +1,8 @@
+def maxDepth(self, root: TreeNode) -> int:
+
+    if not root:
+        return 0
+    
+    left = self.maxDepth(root.left)
+    right = self.maxDepth(root.right)
+    return max(left, right) + 1
