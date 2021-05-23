3a91fb7c9921dc6a8ea8e3f2852b49ec12742e3b
Sungin <jungs@uchicago.edu>
2021-05-22 21:53:00 -0700

    Python_Balanced Binary Tree

---
+++ LeetCode/Python/SecondRound/110_balancedBinaryTree.py
@@ -0,0 +1,14 @@
+def isBalanced(self, root: TreeNode) -> bool:
+
+    return self.checkBalance(root)[1]
+
+def checkBalance(self, root):
+
+    if not root:
+        return (0, True)
+    
+    leftHeight, leftBalanced = self.checkBalance(root.left)
+    rightHeight, rightBalanced = self.checkBalance(root.right)
+
+    return (max(leftHeight, rightHeight) + 1, leftBalanced and rightBalanced and abs(leftHeight - rightHeight) <= 1)
+    
