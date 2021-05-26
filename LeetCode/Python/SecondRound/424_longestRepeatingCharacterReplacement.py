5aa9c6aafe071a7d6f6255664e1a7917ea8bf313
Sungin <jungs@uchicago.edu>
2021-05-26 01:17:14 -0700

    Python_Longest Repeating Character Replacement

---
+++ LeetCode/Python/SecondRound/424_longestRepeatingCharacterReplacement.py
@@ -0,0 +1,19 @@
+import collections
+def characterReplacement(self, s: str, k: int) -> int:
+
+    maxLen = 0
+    start = 0
+    freq = collections.Counter(s)
+    maxCharCount = 0
+
+    for end in range(len(s)):
+        freq[s[end]] += 1
+        maxCharCount = max(maxCharCount, freq[s[end]])
+
+        while (end - start + 1) - maxCharCount > k: #out of operations
+            freq[s[start]] -= 1
+            start += 1
+        
+        maxLen = max(maxLen, end - start + 1)
+
+    return maxLen
