30e84adf800ffcdd8cc83377df7a01fd1792752f
Sungin <jungs@uchicago.edu>
2021-06-01 22:55:09 -0700

    Python_Next Greatest Element 1

---
+++ LeetCode/Python/SecondRound/496_nextGreaterElement1.py
@@ -0,0 +1,14 @@
+def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+    nextGreatest = {}
+    stack = []
+
+    for num in nums2:
+        while stack and stack[-1] < num:
+            nextGreatest[stack.pop()] = num
+        stack.append(num)
+    
+    for i in range(len(nums1)):
+        nums1[i] = nextGreaterElement.get(nums1[i], -1)
+    
+    return nums1
